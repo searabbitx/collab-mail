e138b66a73285c999b490fda584eb02370f320f5
searabbit <46087838+searabbitx@users.noreply.github.com>
2025-10-17 16:44:49 +0200

    Remove redundant method

--- src/main/java/io/searabbitx/ui/Tab.java
+++ src/main/java/io/searabbitx/ui/Tab.java
@@ -31,14 +31,6 @@ public class Tab extends JPanel {
         setupPeriodicTasks();
     }
 
-    private static void selectNextRowIfAvailable(DefaultTableModel model, int selectedRow, JTable table) {
-        int rowCount = model.getRowCount();
-        if (rowCount > 0) {
-            int newSelection = Math.min(selectedRow, rowCount - 1);
-            table.setRowSelectionInterval(newSelection, newSelection);
-        }
-    }
-
     private void setupPeriodicTasks() {
         var timer = new Timer(POLLING_PERIOD_SECONDS * 1000, _ -> pollMessages());
         timer.setRepeats(true);
