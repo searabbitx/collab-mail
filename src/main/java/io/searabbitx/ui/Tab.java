dc1a828a1765b0a0ea90d426e7bb7c06a57c459e
searabbit <46087838+searabbitx@users.noreply.github.com>
2025-10-20 10:32:31 +0200

    Extract messages pane class

--- src/main/java/io/searabbitx/ui/Tab.java
+++ src/main/java/io/searabbitx/ui/Tab.java
@@ -2,8 +2,8 @@ package io.searabbitx.ui;
 
 import io.searabbitx.mail.MailBox;
 import io.searabbitx.ui.pane.AddressPane;
+import io.searabbitx.ui.pane.MessagesPane;
 import io.searabbitx.ui.pane.PollButtonPane;
-import io.searabbitx.ui.table.MessagesTable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,56 +11,42 @@ import java.awt.*;
 public class Tab extends JPanel {
     private static final int POLLING_PERIOD_SECONDS = 5;
 
-    private final MailBox mailBox;
     private final PollButtonPane pollButtonPane;
     private final AddressPane addressPane;
-    private MessagesTable messagesTable;
-    private JButton clearButton;
-    private JButton removeMessageButton;
+    private final MessagesPane messagesPane;
 
     public Tab(MailBox mailBox) {
-        this.mailBox = mailBox;
-        addressPane = new AddressPane(mailBox);
         pollButtonPane = new PollButtonPane();
+        addressPane = new AddressPane(mailBox);
+        messagesPane = new MessagesPane(mailBox);
 
-        initializeComponents();
-        setupLayout();
-        setupEventHandlers();
-        setupPeriodicTasks();
-    }
-
-    private void setupPeriodicTasks() {
-        var timer = new Timer(POLLING_PERIOD_SECONDS * 1000, _ -> pollMessages());
-        timer.setRepeats(true);
-        timer.start();
-    }
-
-    private void initializeComponents() {
-        messagesTable = new MessagesTable(this.mailBox::removeMailAt, this.mailBox::clearMails);
-
-        clearButton = new JButton("Clear");
-        removeMessageButton = new JButton("Remove selected");
+        pollButtonPane.onPollButtonPressed(messagesPane::pollMessages);
 
         restoreValues();
+        setupPeriodicTasks();
+        setupLayout();
     }
 
     private void restoreValues() {
         addressPane.restoreValues();
-        mailBox.mails().forEach(messagesTable::addRow);
+        messagesPane.restoreValues();
+    }
+
+    private void setupPeriodicTasks() {
+        var timer = new Timer(POLLING_PERIOD_SECONDS * 1000, _ -> messagesPane.pollMessages());
+        timer.setRepeats(true);
+        timer.start();
     }
 
     private void setupLayout() {
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
-        var messagesPane = createMessagesPane();
-
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, addressPane.component(), messagesPane);
-        splitPane.setResizeWeight(0.10); // Initially give 75% to left pane
-        splitPane.setOneTouchExpandable(true); // Add expand/collapse buttons
-        splitPane.setContinuousLayout(true); // Smooth resizing
-        splitPane.setDividerSize(8); // Set divider thickness
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, addressPane.component(), messagesPane.component());
+        splitPane.setResizeWeight(0.10);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setContinuousLayout(true);
+        splitPane.setDividerSize(8);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -71,59 +57,9 @@ public class Tab extends JPanel {
 
         var verticalSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, pollButtonPane.component(), splitPane);
         verticalSplit.setResizeWeight(0);
-        verticalSplit.setDividerSize(8); // Set divider thickness
+        verticalSplit.setDividerSize(8);
 
         add(verticalSplit, gbc);
-    }
-
-    private Component createMessagesPane() {
-        var main = new JPanel(new GridBagLayout());
-
-        var gbc = new GridBagConstraints();
-
-        var buttonPanel = new JPanel(new GridBagLayout());
-        var buttonGbc = new GridBagConstraints();
-        buttonGbc.gridy = 0;
-        buttonGbc.gridx = 0;
-        buttonGbc.fill = GridBagConstraints.HORIZONTAL;
-        buttonGbc.anchor = GridBagConstraints.NORTH;
-        buttonGbc.weightx = 1.0;
-        buttonGbc.weighty = 0;
-        buttonGbc.insets = new Insets(5, 0, 5, 5);
-        buttonPanel.add(clearButton, buttonGbc);
-        buttonGbc.gridx = 1;
-        buttonPanel.add(removeMessageButton, buttonGbc);
-
-        gbc.gridy = 0;
-        gbc.gridx = 0;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0;
-        main.add(buttonPanel, gbc);
-
-        JScrollPane messagesScrollPane = new JScrollPane(messagesTable);
-        messagesScrollPane.setMinimumSize(new Dimension(300, 400));
-        messagesScrollPane.setPreferredSize(new Dimension(600, 400));
-        gbc.weighty = 2.0;
-        gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        main.add(messagesScrollPane, gbc);
-
-        main.setBorder(BorderFactory.createTitledBorder("Inbox"));
-
-        return main;
-    }
-
-    private void setupEventHandlers() {
-        clearButton.addActionListener(_ -> messagesTable.clearMessages());
-        removeMessageButton.addActionListener(_ -> messagesTable.removeSelectedEntry());
-
-        pollButtonPane.onPollButtonPressed(this::pollMessages);
-    }
-
-    private void pollMessages() {
-        this.mailBox.pollInteractions().forEach(messagesTable::addRow);
     }
 
 }
---
+++ src/main/java/io/searabbitx/ui/pane/MessagesPane.java
@@ -0,0 +1,76 @@
+package io.searabbitx.ui.pane;
+
+import io.searabbitx.mail.MailBox;
+import io.searabbitx.ui.table.MessagesTable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MessagesPane {
+    private final JPanel component;
+
+    private final JButton clearButton;
+    private final JButton removeButton;
+    private final MessagesTable messagesTable;
+    private final MailBox mailBox;
+
+    public MessagesPane(MailBox mailBox) {
+        this.mailBox = mailBox;
+        component = new JPanel(new GridBagLayout());
+        clearButton = new JButton("Clear");
+        removeButton = new JButton("Remove selected");
+        messagesTable = new MessagesTable(mailBox::removeMailAt, mailBox::clearMails);
+
+        clearButton.addActionListener(_ -> messagesTable.clearMessages());
+        removeButton.addActionListener(_ -> messagesTable.removeSelectedEntry());
+
+        setupLayout();
+    }
+
+    public void pollMessages() {
+        mailBox.pollInteractions().forEach(messagesTable::addRow);
+    }
+
+    public Component component() {
+        return component;
+    }
+
+    public void restoreValues() {
+        mailBox.mails().forEach(messagesTable::addRow);
+    }
+
+    private void setupLayout() {
+        var gbc = new GridBagConstraints();
+
+        var buttonPanel = new JPanel(new GridBagLayout());
+        var buttonGbc = new GridBagConstraints();
+        buttonGbc.gridy = 0;
+        buttonGbc.gridx = 0;
+        buttonGbc.fill = GridBagConstraints.HORIZONTAL;
+        buttonGbc.anchor = GridBagConstraints.NORTH;
+        buttonGbc.weightx = 1.0;
+        buttonGbc.weighty = 0;
+        buttonGbc.insets = new Insets(5, 0, 5, 5);
+        buttonPanel.add(clearButton, buttonGbc);
+        buttonGbc.gridx = 1;
+        buttonPanel.add(removeButton, buttonGbc);
+
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 1.0;
+        gbc.weighty = 0;
+        component.add(buttonPanel, gbc);
+
+        JScrollPane messagesScrollPane = new JScrollPane(messagesTable);
+        messagesScrollPane.setMinimumSize(new Dimension(300, 400));
+        messagesScrollPane.setPreferredSize(new Dimension(600, 400));
+        gbc.weighty = 2.0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        component.add(messagesScrollPane, gbc);
+
+        component.setBorder(BorderFactory.createTitledBorder("Inbox"));
+    }
+}
